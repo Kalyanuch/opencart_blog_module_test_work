55c0d5e66c55261626b7db8d88bc79294e6350f1
Shamrylo Mykola <itlutsk@gmail.com>
2022-12-02 18:37:28 +0200

    Initial commit.

---
+++ admin/language/en-gb/extension/module/account_blog.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Account Blog';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified account blog module!';
+$_['text_edit']        = 'Edit Account Blog Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify account blog module!';
